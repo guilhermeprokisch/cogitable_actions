24bd9bd97a17befdb7f4222076cb38d029d9b928
Guilherme <guilherme.prokisch@gmail.com>
2022-03-14 17:39:44 -0300

    chore: add .eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,11 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'standard'
-  ],
+  root: true,
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
   plugins: [
-    '@typescript-eslint'
+    '@typescript-eslint',
   ],
-  rules: {
-  }
-}
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
