50e5bddc1dbf81e108c3a713dbdf8d47609bc89e
Guilherme <guilherme.prokisch@gmail.com>
2022-03-14 20:08:24 -0300

    fix: ncc build params

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationFunction } from 'probot'
-import { cogitable } from './app'
-
-declare function run(
-  probotApp: ApplicationFunction | ApplicationFunction[]
-): Promise<void>;
-
-run(cogitable)
