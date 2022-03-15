91cd07adb534081fc9155407cc89e703a9d510a8
Guilherme <guilherme.prokisch@gmail.com>
2022-03-15 14:55:53 -0300

    feat: ensure do nothing when a issue is opened by a bot

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,19 @@ export const app = (probot: Probot): void => {
   probot.on(
     [
       'issues.opened',
+      'issues.edited',
       'issue_comment.created',
-      'issue_comment.edited',
-      'issues.edited'
+      'issue_comment.edited'
     ],
     async context => {
+      if (context.payload.sender.type === 'Bot') {
+        return Promise.resolve()
+      }
+
       const issueComment = context.issue({
         body: 'Thanks for opening this issue!'
       })
+
       await context.octokit.issues.createComment(issueComment)
     }
   )
