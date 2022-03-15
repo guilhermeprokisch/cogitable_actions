9c84d21e781c08ae4d2a9e9ff26cdcb71363b1fa
Guilherme <guilherme.prokisch@gmail.com>
2022-03-14 23:30:50 -0300

    refactor: renamed adater import to adapaterGihubAction

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import run from '@probot/adapter-github-actions'
+import adapterGithubActions from '@probot/adapter-github-actions'
 import { app } from './app'
 
-run.run(app)
+adapterGithubActions.run(app)
