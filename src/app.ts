0896c9948835fd3b342347a558861a5616472b0e
Guilherme <guilherme.prokisch@gmail.com>
2022-03-15 12:04:07 -0300

    test: created fake test

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import { Probot } from 'probot'
 
 export const app = (probot: Probot): void => {
-  probot.on('issues.opened', async context => {
-    const issueComment = context.issue({
-      body: 'Thanks for opening this issue!'
-    })
-    await context.octokit.issues.createComment(issueComment)
-  })
+  probot.on(
+    [
+      'issues.opened',
+      'issue_comment.created',
+      'issue_comment.edited',
+      'issues.edited'
+    ],
+    async context => {
+      const issueComment = context.issue({
+        body: 'Thanks for opening this issue!'
+      })
+      await context.octokit.issues.createComment(issueComment)
+    }
+  )
 }
