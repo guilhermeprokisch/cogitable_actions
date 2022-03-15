33c27b302ca5d7f8dd150ea2905cf40b480ddc8b
Guilherme <guilherme.prokisch@gmail.com>
2022-03-15 16:41:44 -0300

    test: ensure doubleBrackets returns true if theres double brackets

--- src/utils/doubleBrackts.spec.ts
+++ src/utils/doubleBrackts.spec.ts
@@ -1,14 +1,29 @@
 import { DoubleBrackts } from './doubleBrackts'
 
-const negative_cases = [
+const negativeCases = [
   { value: 'Test of body without brackts', expected: false },
   { value: 'One more comment whitout brackts', expected: false },
   { value: 'Test test test [ test', expected: false }
 ]
 
+const positiveCases = [
+  { value: 'Test of string [[with]] double brackets', expected: true },
+  { value: '[[One more]] test', expected: true },
+  { value: '[[Brackts]] again test [[Second]]', expected: true }
+]
+
 describe('DoubleBrackts', () => {
-  it.each(negative_cases)(
-    'Should contains method return false if there`s not double brackets',
+  it.each(negativeCases)(
+    'Should contains method return false if there is not double brackets',
+    ({ value, expected }) => {
+      const doubleBrackts = new DoubleBrackts(value)
+      const itContains = doubleBrackts.contains()
+      expect(itContains).toBe(expected)
+    }
+  )
+
+  it.each(positiveCases)(
+    'Should contains method return true if there is double brackets',
     ({ value, expected }) => {
       const doubleBrackts = new DoubleBrackts(value)
       const itContains = doubleBrackts.contains()
