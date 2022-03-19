842b2f39b2b7852354a9159c270f06bf2894a645
Guilherme <guilherme.prokisch@gmail.com>
2022-03-19 18:25:45 -0300

    refactor: moved BrackTermSearch for it's own file

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,7 @@
 import { Probot } from 'probot'
 import _ from 'lodash'
 import { DoubleBracktsHandler } from './utils/doubleBrackts'
-
-class BrackTermsSearch {
-  terms: string[]
-  context: any
-
-  constructor (terms: string[], context: any) {
-    this.terms = terms
-    this.context = context
-  }
-
-  private async searchTerm (term: string): Promise<any> {
-    const results = await this.context.octokit.search.issuesAndPullRequests(
-      this.context.repo({
-        q: term + `repo:${this.context.repo().owner}`,
-        order: 'asc',
-        per_page: 1
-      })
-    )
-    return results
-  }
-
-  private parseResult (term:any, rawResult:any):any {
-    // console.log(rawResult.data.items[0])
-
-    return {
-      term: term,
-      number: rawResult.data.items[0] ? rawResult.data.items[0].number : null,
-      title: rawResult.data.items[0] ? rawResult.data.items[0].title : null
-    }
-  }
-
-  async search () {
-    const rawResult = await Promise.all(this.terms.map(async (term) => await this.searchTerm(term)))
-    return rawResult.map((result, index) => this.parseResult(this.terms[index], result))
-  }
-}
+import { BrackTermsSearch } from './bracktermssearch'
 
 export const app = (probot: Probot): void => {
   probot.on(
@@ -51,9 +16,12 @@ export const app = (probot: Probot): void => {
       if (context.payload.sender.type === 'Bot') {
         return
       }
-      // there's a issue here with protobot types
+
+      // ts-ingore due a issue with protobot types
       // @ts-ignore
-      const body = context.payload.comment ? context.payload.comment.body : context.payload.issue.body
+      const body = context.payload.comment // @ts-ignore
+        ? context.payload.comment.body
+        : context.payload.issue.body
 
       const doubleBracktsHandler = new DoubleBracktsHandler(body)
 
@@ -62,7 +30,10 @@ export const app = (probot: Probot): void => {
       }
 
       const bracketTerms = doubleBracktsHandler.extract()
-      const searchResults = await new BrackTermsSearch(bracketTerms, context).search()
+      const searchResults = await new BrackTermsSearch(
+        bracketTerms,
+        context
+      ).search()
 
       // console.log(searchResults)
 
---
+++ src/bracktermssearch.ts
@@ -0,0 +1,35 @@
+export class BrackTermsSearch {
+  terms: string[]
+  context: any
+
+  constructor (terms: string[], context: any) {
+    this.terms = terms
+    this.context = context
+  }
+
+  private async searchTerm (term: string): Promise<any> {
+    const results = await this.context.octokit.search.issuesAndPullRequests(
+      this.context.repo({
+        q: term + `repo:${this.context.repo().owner}`,
+        order: 'asc',
+        per_page: 1
+      })
+    )
+    return results
+  }
+
+  private parseResult (term:any, rawResult:any):any {
+    // console.log(rawResult.data.items[0])
+
+    return {
+      term: term,
+      number: rawResult.data.items[0] ? rawResult.data.items[0].number : null,
+      title: rawResult.data.items[0] ? rawResult.data.items[0].title : null
+    }
+  }
+
+  async search () {
+    const rawResult = await Promise.all(this.terms.map(async (term) => await this.searchTerm(term)))
+    return rawResult.map((result, index) => this.parseResult(this.terms[index], result))
+  }
+}
