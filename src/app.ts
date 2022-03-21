57d1a0839ca02d871cb0120c151f48cf63943d94
Guilherme <guilherme.prokisch@gmail.com>
2022-03-20 23:35:33 -0300

    refactor: moved IssueCreator for it's own file

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,7 @@
 import { Probot } from 'probot'
 import { DoubleBracktsHandler } from './utils/doubleBrackts'
 import { BrackTermsSearch } from './bracktermssearch'
-import { SearchResult } from './types'
-
-class IssueCreator {
-  searchResults: SearchResult[]
-  context: any
-
-  constructor (searchResults: SearchResult[], context: any) {
-    this.searchResults = searchResults
-    this.context = context
-  }
-
-  private async createNewIssue (result: SearchResult) {
-    if (result.number) {
-      return result
-    }
-
-    const newIssue = await this.context.octokit.rest.issues.create(
-      this.context.repo({
-        title: result.term,
-        body: ' '
-      })
-    )
-    result.number = newIssue.data.number
-    result.title = newIssue.data.title
-    return result
-  }
-
-  async create (): Promise<SearchResult[]> {
-    return await Promise.all(this.searchResults.map(async (result) => await this.createNewIssue(result)))
-  }
-}
+import { IssueCreator } from './issuecreator'
 
 export const app = (probot: Probot): void => {
   probot.on(
---
+++ src/issuecreator.ts
@@ -0,0 +1,31 @@
+import { SearchResult } from './types'
+
+export class IssueCreator {
+  searchResults: SearchResult[]
+  context: any
+
+  constructor (searchResults: SearchResult[], context: any) {
+    this.searchResults = searchResults
+    this.context = context
+  }
+
+  private async createNewIssue (result: SearchResult) {
+    if (result.number) {
+      return result
+    }
+
+    const newIssue = await this.context.octokit.rest.issues.create(
+      this.context.repo({
+        title: result.term,
+        body: ' '
+      })
+    )
+    result.number = newIssue.data.number
+    result.title = newIssue.data.title
+    return result
+  }
+
+  async create (): Promise<SearchResult[]> {
+    return await Promise.all(this.searchResults.map(async (result) => await this.createNewIssue(result)))
+  }
+}
